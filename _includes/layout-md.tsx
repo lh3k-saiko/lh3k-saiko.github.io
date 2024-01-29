30c7abd9ade3631e8c785c67728c0888eb83b214
lh3k <NOIZEBREAKSEQUENCE@126.com>
2024-01-30 01:33:59 +0800

    update

--- _includes/layout-md.tsx
+++ _includes/layout-md.tsx
@@ -2,7 +2,7 @@ import Layout from "./layout.tsx";
 
 export default ({ children, ...data }: Lume.Data, helpers: Lume.Helpers ) => {
   children = (
-    <article class="content-display mx-auto px-6 box-border max-w-5xl"> {children} </article>
+    <article class="content-display mx-auto px-6 box-border max-w-[52rem]"> {children} </article>
   );
 
   return Layout({ children, ...data }, helpers);
